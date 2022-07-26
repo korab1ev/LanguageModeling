233fd86d2c82d2079fbf20216429d5927919d2e7
Maxim Korablev <mkorablev.apm@gmail.com>
2022-07-26 14:46:58 +0300

    added vocab.py

---
+++ RNN-name-generator-samsung/vocab.py
@@ -0,0 +1,100 @@
+import sys
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+# Reference: https://github.com/yandexdataschool/nlp_course/blob/2021/week04_seq2seq/vocab.py
+
+class Vocab:
+    def __init__(self, tokens, bos="_BOS_", eos="_EOS_", unk='_UNK_'):
+        """
+        A special class that converts lines of tokens into matrices and backwards
+        """
+        assert all(tok in tokens for tok in (bos, eos, unk))
+        self.tokens = tokens
+        self.token_to_ix = {t:i for i, t in enumerate(tokens)}
+        self.bos, self.eos, self.unk = bos, eos, unk
+        self.bos_ix = self.token_to_ix[bos]
+        self.eos_ix = self.token_to_ix[eos]
+        self.unk_ix = self.token_to_ix[unk]
+
+    def __len__(self):
+        return len(self.tokens)
+
+    @staticmethod
+    def from_lines(lines, bos="_BOS_", eos="_EOS_", unk='_UNK_'):
+        flat_lines = '\n'.join(list(lines)).split()
+        tokens = sorted(set(flat_lines))
+        tokens = [t for t in tokens if t not in (bos, eos, unk) and len(t)]
+        tokens = [bos, eos, unk] + tokens
+        return Vocab(tokens, bos, eos, unk)
+
+    def tokenize(self, string):
+        """converts string to a list of tokens"""
+        tokens = [tok if tok in self.token_to_ix else self.unk
+                  for tok in string.split()]
+        return [self.bos] + tokens + [self.eos]
+
+    def to_matrix(self, lines, dtype=torch.int64, max_len=None):
+        """
+        convert variable length token sequences into  fixed size matrix
+        example usage:
+        words to ids (0 = bos, 1 = eos):
+        
+        >>>batch_ids = inp_voc.to_matrix(batch_lines)
+        >>>print("words to ids (0 = bos, 1 = eos):")
+        >>>print(batch_ids)
+        words to ids (0 = bos, 1 = eos):
+        tensor([[   0, 2688, 2943, 1108,   29,    1,    1,    1],
+        [   0, 2922, 1834, 8035,   59, 3800,   29,    1],
+        [   0, 6030, 2083,   29,    1,    1,    1,    1],
+        [   0, 4927, 1870,   29,    1,    1,    1,    1],
+        [   0, 5549, 1453,   27,  592,   29,    1,    1]])
+        """
+        lines = list(map(self.tokenize, lines))
+        max_len = max_len or max(map(len, lines))
+
+        matrix = torch.full((len(lines), max_len), self.eos_ix, dtype=dtype)
+        for i, seq in enumerate(lines):
+            row_ix = list(map(self.token_to_ix.get, seq))[:max_len]
+            matrix[i, :len(row_ix)] = torch.as_tensor(row_ix)
+
+        return matrix
+
+    def to_lines(self, matrix, crop=True):
+        """
+        Convert matrix of token ids into strings
+        :param matrix: matrix of tokens of int32, shape=[batch,time]
+        :param crop: if True, crops BOS and EOS from line
+        :return:
+        """
+        lines = []
+        for line_ix in map(list,matrix):
+            if crop:
+                if line_ix[0] == self.bos_ix:
+                    line_ix = line_ix[1:]
+                if self.eos_ix in line_ix:
+                    line_ix = line_ix[:line_ix.index(self.eos_ix)]
+            line = ' '.join(self.tokens[i] for i in line_ix)
+            lines.append(line)
+        return lines
+    
+    def compute_mask(self, input_ix):
+        """ compute a boolean mask that equals "1" until first EOS (including that EOS) """
+        return F.pad(torch.cumsum(input_ix == self.eos_ix, dim=-1)[..., :-1] < 1, pad=(1, 0, 0, 0), value=True)
+
+        """
+        >>>dummy_out_tokens
+        tensor([[   0, 2688, 2943, 1108,   29,    1,    1,    1],
+        [   0, 2922, 1834, 8035,   59, 3800,   29,    1],
+        [   0, 6030, 2083,   29,    1,    1,    1,    1],
+        [   0, 4927, 1870,   29,    1,    1,    1,    1],
+        [   0, 5549, 1453,   27,  592,   29,    1,    1]])
+        mask = out_voc.compute_mask(dummy_out_tokens)
+        >>>mask
+        tensor([[ True,  True,  True,  True,  True,  True, False],
+        [ True,  True,  True,  True,  True,  True, False],
+        [ True,  True,  True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True,  True, False],
+        [ True,  True,  True,  True,  True,  True, False]])
+        """
